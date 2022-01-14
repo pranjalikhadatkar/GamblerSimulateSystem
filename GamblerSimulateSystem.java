012fc3c5877572c06a9f9622ce201d5425bccf94
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-15 01:31:04 +0530

    Added: total amount after 20 days of playing won or lost

--- GamblerSimulateSystem.java
+++ GamblerSimulateSystem.java
@@ -8,6 +8,7 @@ public class GamblerSimulateSystem {
 	static int BET_AMOUNT=1;
 	static int WON=1;
 	static int LOST=0;
+	static int TOTAL_AMOUNT=0;
 	
 	public static void main(String[] args) {
 		 Scanner sc = new Scanner(System.in);
@@ -15,30 +16,34 @@ public class GamblerSimulateSystem {
 		 System.out.println("Amount to bet after every play "+BET_AMOUNT);
 		 int currentcashofplayer = AMOUNT_OF_STAKE;
 		 
-		 while(currentcashofplayer > 50 && currentcashofplayer < 150) {
-			 int play = (int) Math.floor(Math.random() * 10) % 2;
-			 System.out.println("Gamblers play:"+play);
+		 for (int i = 1; i <= 20; i++) {
+			 while(currentcashofplayer > 50 && currentcashofplayer < 150) {
+				 int play = (int) Math.floor(Math.random() * 10) % 2;
+				 System.out.println("Player play:"+play);
 		 
 			 switch(play) {
 		 		case 1:
-		 			currentcashofplayer = currentcashofplayer + BET_AMOUNT;
-		 			System.out.println("Total current cash gambler has "+currentcashofplayer);
-		 			 System.out.println("Player won the current play");
-		 			break;
-		 		case 0:
-		 			currentcashofplayer = currentcashofplayer - BET_AMOUNT;
-		 			System.out.println("Total current cash gambler has "+currentcashofplayer);
-		 			 System.out.println("Player lost the current play");
-		 			break;
+		 		    currentcashofplayer = currentcashofplayer + BET_AMOUNT;
+		 			System.out.println("Total current cash Player has "+currentcashofplayer);
+		 			System.out.println("Player won the current play");
+		 				break;
+		 		case 2:
+		 				currentcashofplayer = currentcashofplayer - BET_AMOUNT;
+		 				System.out.println("Total current cash Player has "+currentcashofplayer);
+		 				 System.out.println("Player lost the current play");
+		 				break;
 		 		default:
-		 			System.out.println("default");
+		 				System.out.println("default");
+				 }
 			 }
-			 
-		 }
-		 if(currentcashofplayer==150 || currentcashofplayer==50) {
-			 System.out.println("Gambler Quit the game for the day");
-		 }
+			 if(currentcashofplayer==150 || currentcashofplayer==50) {
+				 System.out.println("Player Quit the game for the day" + i);
+				 
+			 }
+			 			 TOTAL_AMOUNT = currentcashofplayer;
+		}
 		 sc.close();
+		 System.out.println("Total amount player has after 20 days: " + TOTAL_AMOUNT+" $");
 	}
-
+	
 }
