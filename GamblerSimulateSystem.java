5504bf48029d1aefb5ef2bbafe9e55e90333749e
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-15 01:11:13 +0530

    Added: Welcome to gambler simulation

---
+++ GamblerSimulateSystem.java
@@ -0,0 +1,13 @@
+package com.GamblerSimulateSystem;
+
+public class GamblerSimulateSystem {
+
+	static int money=100;
+	static int betmoney=1;
+	
+	public static void main(String[] args) {
+		System.out.println("Amount of stake is $" + money);
+		System.out.println("Bet amount is $"+ betmoney);
+	}
+
+}
